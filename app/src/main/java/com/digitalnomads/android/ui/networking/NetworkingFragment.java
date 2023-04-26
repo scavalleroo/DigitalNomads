10ab12c21439b97de3c85dea85790d89099c73a1
Alessandro Cavallotti <alessandro@junto.space>
2023-04-27 00:44:45 +0200

    New features

--- app/src/main/java/com/digitalnomads/android/ui/networking/NetworkingFragment.java
+++ app/src/main/java/com/digitalnomads/android/ui/networking/NetworkingFragment.java
@@ -2,6 +2,8 @@ package com.digitalnomads.android.ui.networking;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.digitalnomads.android.R;
 
@@ -25,7 +30,34 @@ public class NetworkingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_netwroking, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_netwroking, container, false);
+        // Reference the UI element using findViewById()
+        Button filter = (Button) view.findViewById(R.id.button_filter_networking);
+
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View popupView = LayoutInflater.from(view.getContext()).inflate(R.layout.filter_popup, null);
+                ImageView closeButton = popupView.findViewById(R.id.close_popup);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setView(popupView);
+                AlertDialog filterPopup = builder.create();
+
+                // Set an OnClickListener on the close button to dismiss the popup
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        filterPopup.dismiss();
+                    }
+                });
+
+                filterPopup.show();
+            }
+        });
+
+        return view;
     }
 
     @Override

2866371964dddbf08111be0f1e1a924911502957
Alessandro Cavallotti <alessandro@junto.space>
2023-04-26 12:53:18 +0200

    Project Structure

---
+++ app/src/main/java/com/digitalnomads/android/ui/networking/NetworkingViewModel.java
@@ -0,0 +1,7 @@
+package com.digitalnomads.android.ui.networking;
+
+import androidx.lifecycle.ViewModel;
+
+public class NetworkingViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/digitalnomads/android/ui/places/PlaceViewModel.java
@@ -0,0 +1,7 @@
+package com.digitalnomads.android.ui.places;
+
+import androidx.lifecycle.ViewModel;
+
+public class PlaceViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

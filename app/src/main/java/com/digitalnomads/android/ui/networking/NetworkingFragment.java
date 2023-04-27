eb674457b973e7f8f67e9f65823e33b149cf83ec
Alessandro Cavallotti <alessandro@junto.space>
2023-04-27 11:16:14 +0200

    People working

--- app/src/main/java/com/digitalnomads/android/ui/networking/NetworkingFragment.java
+++ app/src/main/java/com/digitalnomads/android/ui/networking/NetworkingFragment.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,9 +16,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.digitalnomads.android.R;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class NetworkingFragment extends Fragment {
 
@@ -32,6 +37,28 @@ public class NetworkingFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_netwroking, container, false);
+
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.list_working_people);
+
+        List<Integer> ids = Arrays.asList(R.drawable.clara_rodriguez, R.drawable.maria_gracia, R.drawable.monica_rocamura, R.drawable.javier_fernandez, R.drawable.lucia_smith, R.drawable.mark_brown);
+
+        int widthInDp = 350;
+        float scale = getResources().getDisplayMetrics().density;
+        int widthInPixels = (int) (widthInDp * scale + 0.5f);
+
+        for (int i = 0; i < ids.size(); i++) {
+            View card_working_person = inflater.inflate(R.layout.card_working_person, null);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    widthInPixels, // width in pixels or dp
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            card_working_person.setLayoutParams(layoutParams);
+
+            ImageView image = (ImageView) card_working_person.findViewById(R.id.imageWorkingPartner);
+            image.setImageDrawable(getResources().getDrawable(ids.get(i), null));
+            linearLayout.addView(card_working_person);
+        }
+
         // Reference the UI element using findViewById()
         Button filter = (Button) view.findViewById(R.id.button_filter_networking);
 
